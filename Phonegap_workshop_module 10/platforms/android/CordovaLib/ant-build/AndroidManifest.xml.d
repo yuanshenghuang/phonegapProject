fe4ca6fb28340e8c8a6cbd0cb67a01a0b427c28a
yuanshenghuang <yuansheng@live.be>
2015-02-04 14:31:50 +0100

    project

---
+++ Phonegap_workshop_module 10/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+D:\CVO 2014 2015\Programmeren 5\workshop\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
+D:\CVO 2014 2015\Programmeren 5\workshop\platforms\android\CordovaLib\AndroidManifest.xml \
---
+++ Phonegap_workshop_module 10/platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+D:\CVO 2014 2015\Programmeren 5\workshop\platforms\android\ant-build\CordovaApp-debug-unaligned.apk : \
+D:\CVO 2014 2015\Programmeren 5\workshop\platforms\android\ant-build\CordovaApp.ap_ \
+D:\CVO 2014 2015\Programmeren 5\workshop\platforms\android\ant-build\classes.dex \
+D:\CVO 2014 2015\Programmeren 5\workshop\platforms\android\CordovaLib\ant-build\classes.jar \
