ae793603f9ef4c40c495a92e61ce1673b33b12ae
yuanshenghuang <yuansheng@live.be>
2015-02-04 14:36:33 +0100

    project2

---
+++ www/js/EmployeeView.js
@@ -0,0 +1,15 @@
+// JavaScript source code
+var EmployeeView = function (employee) {
+
+    this.initialize = function () {
+        this.$el = $('<div/>');
+    };
+
+    this.render = function () {
+        this.$el.html(this.template(employee));
+        return this;
+    };
+
+    this.initialize();
+
+}
