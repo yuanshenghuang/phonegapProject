c5d993d883a16d3721f16ef422bc78ed73c42f7b
yuanshenghuang <yuansheng@live.be>
2015-02-04 14:35:23 +0100

    delete

--- Phonegap_workshop_module 10/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-D:\CVO 2014 2015\Programmeren 5\workshop\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
-D:\CVO 2014 2015\Programmeren 5\workshop\platforms\android\CordovaLib\AndroidManifest.xml \
